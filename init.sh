76b055b5ef08745a2c5420972023f49727695499
root <root@test-github-clone.c.mycloud-1055.internal>
2017-11-04 04:44:57 +0000

    Fix google gcp containers restart problem, close #2

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ set -e
 # Preload iptables,Rule lost when preventing restart of container!
 iptables-restore < /etc/sysconfig/iptables
 
-# Repair gcp container restart, can not access google family bucket(disable pmtu discovery)
+# Repair gcp container restart, can not access google family bucket(Disable pmtu discovery!)
 sysctl -w net.ipv4.ip_no_pmtu_disc=1
 
 
